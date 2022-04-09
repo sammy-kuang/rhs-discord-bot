c52dcb104ef8aebdcf80f91aab46212c6a063b79
Eric Xiao <ericxiao0000@gmail.com>
2022-04-09 10:21:18 -0700

    now actually sends notification, list command lists all notifications

--- bot.py
+++ bot.py
@@ -7,8 +7,28 @@ from asyncio import sleep
 
 bot = commands.Bot(command_prefix='>', help_command=None)
 
-cur_day_of_week = '3'
-cur_time = '0200'
+cur_day_of_week = ''
+cur_time = ''
+
+@bot.command()
+async def list(ctx, *args):
+    f = open(get_guild_file(ctx.guild))
+    data = json.load(f)
+
+    msg = ''
+    if len(args) == 0 or args[0] != 'all':
+        for noti in data['notifications']:
+            if noti['channel_id'] == ctx.channel.id:
+                msg += noti['message']
+                msg += '\n'
+    else:
+        for noti in data['notifications']:
+            msg += noti['message']
+            msg += '\n'
+            
+    f.close()
+
+    await ctx.send(msg)
 
 @bot.command()
 async def help(ctx):
@@ -55,29 +75,39 @@ async def on_ready():
     print('Servers (guilds): ')
     for guild in bot.guilds:
         print(' - ' + guild.name + '  id: ' + str(guild.id))
+        f = open(get_guild_file(guild), 'a+')
+        # asoejoaej
+        f.close()
+
     await bot.change_presence(status=discord.Status.dnd, activity=discord.Game('>?>>'))
     update_task.before_loop(bot.wait_until_ready)
     update_task.start()
+
 
 # updates
 @tasks.loop(seconds=1)
 async def update_task():
     while True:
-        await update_cur_time()
-        await check_notifications()
-        await sleep(1)
+        if time.localtime().tm_sec == 0:
+            await update_cur_time()
+            await check_notifications()
+            await sleep(55)
+        await sleep(0.5)
 
 async def update_cur_time():
-    pass
+    global cur_day_of_week, cur_time
+    now = time.localtime()
+    cur_day_of_week = str(now.tm_wday)
+    cur_time = time.strftime('%H%M', now)
 
 async def check_notifications():
     for guild in bot.guilds:
         f = open(get_guild_file(guild))
-
         notis = json.load(f)
+
         for noti in notis['notifications']:
-            # check if time matches now
-            pass
+            if noti['day_of_week'] == cur_day_of_week and noti['time'] == cur_time:
+                await bot.get_channel(noti['channel_id']).send(noti['message'])
         f.close()
 
 def get_guild_file(guild):
