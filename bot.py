20404ca09d6416441294d5bfba33c1ea0213f619
Eric Xiao <ericxiao0000@gmail.com>
2022-04-16 16:12:37 -0700

    refactored command methods to not rely on discord context object, set up bot to work with slash commands (doesnt work yet as discord.py only supports slash commands in unstable release)

--- bot.py
+++ bot.py
@@ -1,17 +1,22 @@
 import discord
+from discord import app_commands
 import json
 import time
 import os.path
 from discord.ext import commands, tasks
 from asyncio import sleep
-# import env
+import env
 
-bot = commands.Bot(command_prefix='>', help_command=None, status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name="everyone"))
+intents = discord.Intents.all()
+
+# bot = commands.Bot(intents=intents,command_prefix='>', help_command=None, status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name="everyone"))
+
+bot = discord.Client(intents=intents)
 
 days_of_week = {'monday': '0', 'tuesday': '1', 'wednesday': '2', 'thursday': '3', 'friday': '4', 'saturday': '5', 'sunday': '6'}
 
 # helper methods
-def has_perms(guild, user):
+def has_perms(guild: discord.Guild, user: discord.Member):
 
     f = open(get_guild_file(guild))
 
@@ -28,15 +33,22 @@ def has_perms(guild, user):
         
     return False
 
-def get_guild_file(guild):
+def get_guild_file(guild: discord.Guild):
     return 'Guilds/' + str(guild.id) + '.json'
 
-async def create_guild_json(guild):
+async def create_guild_json(guild: discord.Guild):
     f = open(get_guild_file(guild), 'w')
     default_data = {'utc_offset': '+00', 'perms_role': 'Add Notis', 'notifications': []}
     f.write(json.dumps(default_data, indent=2))
     f.close()
 
+
+def getIds():
+    ids = []
+    for guild in bot.guilds:
+        ids.append(guild.id)
+    
+    return ids
 
 # bot event and updates
 
@@ -78,7 +90,7 @@ async def check_notifications():
         f.close()
 
 @bot.event
-async def on_guild_join(guild):
+async def on_guild_join(guild: discord.Guild):
     # create guild file and roles on join
     print('joined', guild)
     if not os.path.exists(get_guild_file(guild)):
@@ -97,32 +109,29 @@ async def on_ready():
 
 
 # commands 
-
-@bot.command()
-async def help(ctx, *args):
+def on_help(command: str):
     em = discord.Embed()
-    if len(args) == 0:
+    f = open('command_help_info.json')
+    command_info: dict = json.load(f)
+    if command == '':
         cmds = ''
-        for cmd in bot.commands:
+        for cmd in command_info:
             cmds += ' - '
-            cmds += cmd.name
+            cmds += cmd
             cmds += '\n'
         em.add_field(name='Commands', value=cmds, inline=False)
     else:
-        f = open('command_help_info.json')
-        command_info = json.load(f)
-        if args[0] in command_info:
-            em.add_field(name='Command: ' + args[0], value=command_info[args[0]], inline=False)
+        if command in command_info:
+            em.add_field(name='Command: ' + command, value=command_info[command], inline=False)
         else:
-            em.add_field(name='Command: ' + args[0], value='command not found', inline=False)
-        f.close()
+            em.add_field(name='Command: ' + command, value='command not found', inline=False)
 
-    await ctx.send(embed=em)
+    f.close()
+    return em
 
-@bot.command()
-async def now(ctx):
+async def on_now(guild: discord.Guild):
 
-    f = open(get_guild_file(ctx.guild))
+    f = open(get_guild_file(guild))
     data = json.load(f)
     f.close()
 
@@ -136,17 +145,16 @@ async def now(ctx):
     em = discord.Embed()
     em.add_field(name="Time now", value=time.strftime('%Y-%m-%d %H:%M', guild_time), inline=False)
 
-    await ctx.send(embed=em)
+    return em
 
-@bot.command()
-async def list(ctx, *args):
-    f = open(get_guild_file(ctx.guild))
+async def on_list(guild: discord.Guild, channel: discord.TextChannel, all: bool):
+    f = open(get_guild_file(guild))
     data = json.load(f)
 
     msg = ''
 
     for noti in data['notifications']:
-        if noti['channel_id'] == ctx.channel.id or len(args) > 0 and args[0] == 'all':
+        if noti['channel_id'] == channel.id or all:
             msg += ' - '
             msg += noti['message']
             msg += ' ('
@@ -165,81 +173,74 @@ async def list(ctx, *args):
 
     em = discord.Embed()
     em.add_field(name='Notifications', value=msg, inline=False)
-    await ctx.send(embed=em)
+    return em
 
-@bot.command()
-async def set_utc_offset(ctx, offset):
+async def on_set_utc_offset(guild: discord.Guild, author: discord.Member, offset: str):
 
-    if not has_perms(ctx.guild, ctx.author):
+    if not has_perms(guild, author):
         em = discord.Embed()
         em.add_field(name='Set_utc_offset', value='You have no permissions to use this command')
-        await ctx.send(embed=em)
-        return
+        return em
 
     if len(offset) !=  3 or (offset[0] != '+' and offset[0] != '-'):
         em = discord.Embed()
         em.add_field(name='Set_utc_offset', value='Invalid format')
-        await ctx.send(embed=em)
-        return
+        return em
     if int(offset[1:3]) > (14 if offset[0] == '+' else 12):
         em = discord.Embed()
         em.add_field(name='Set_utc_offset', value='Invalid offset, range is -12 to +14')
-        await ctx.send(embed=em)
-        return
+        return em
 
-    f = open(get_guild_file(ctx.guild))
+    f = open(get_guild_file(guild))
     data = json.load(f)
     f.close()
 
     data['utc_offset'] = offset
 
-    f = open(get_guild_file(ctx.guild), 'w')
+    f = open(get_guild_file(guild), 'w')
     f.write(json.dumps(data, indent=2))
     f.close()
 
     em = discord.Embed()
     em.add_field(name='UTC offset', value='UTC offset is now **' + offset + ':00**')
 
-    await ctx.send(embed=em)
+    return em
 
-@bot.command()
-async def set_perms_role(ctx, role_name):
+async def on_set_perms_role(guild: discord.Guild, author: discord.Member, role_name: str):
 
-    if not has_perms(ctx.guild, ctx.author):
+    if not has_perms(guild, author):
         em = discord.Embed()
         em.add_field(name='Set_perms_role', value='You have no permissions to use this command')
-        await ctx.send(embed=em)
-        return
+        return em
 
-    f = open(get_guild_file(ctx.guild))
+    f = open(get_guild_file(guild))
     data = json.load(f)
     f.close()
 
     data['perms_role'] = role_name
 
-    f = open(get_guild_file(ctx.guild), 'w')
+    f = open(get_guild_file(guild), 'w')
     f.write(json.dumps(data, indent=2))
     f.close()
 
     em = discord.Embed()
     em.add_field(name='Permission Role', value='Role with perms is now: **' + role_name + '**')
 
-    await ctx.send(embed=em)
+    return em
 
 # adds notification info to guild json
-@bot.command()
-async def add(ctx, pings, msg, day, time):
-    if not os.path.exists(get_guild_file(ctx.guild)):
-        create_guild_json(ctx.guild)
+async def on_add(guild: discord.Guild, author: discord.Member, channel: discord.TextChannel, 
+pings: str, msg: str, day: str, time: str):
+    if not os.path.exists(get_guild_file(guild)):
+        create_guild_json(guild)
 
-    f = open(get_guild_file(ctx.guild))
+    f = open(get_guild_file(guild))
     data = json.load(f)
 
-    if not has_perms(ctx.guild, ctx.author):
+    if not has_perms(guild, author):
         em = discord.Embed()
         em.add_field(name='Add', value='You have no permissions to use this command')
-        await ctx.send(embed=em)
-        return
+        return em
 
     day = day.lower()
     
@@ -247,51 +248,46 @@ async def add(ctx, pings, msg, day, time):
     if len(time) != 5 or time[2] != ':':
         em = discord.Embed()
         em.add_field(name='Add', value='Invalid format')
-        await ctx.send(embed=em)
-        return
+        return em
     if int(time[0:2]) > 23 or int(time[3:5]) > 59:
         em = discord.Embed()
         em.add_field(name='Add', value='Invalid time')
-        await ctx.send(embed=em)
-        return 
+        return em
     
     if day not in days_of_week:
         em = discord.Embed()
         em.add_field(name='Add', value='Invalid day of week')
-        await ctx.send(embed=em)
-        return 
+        return em
 
     noti = {
         'pings': pings.split(' ') if len(pings) != 0 else [],
         'message': msg,
         'day_of_week': days_of_week[day],
         'time': time,
-        'channel_id': ctx.channel.id
+        'channel_id': channel.id
     }
 
     f.close()
 
     data['notifications'] += [noti]
 
-    f = open(get_guild_file(ctx.guild), 'w')
+    f = open(get_guild_file(guild), 'w')
     f.write(json.dumps(data, indent=2))
     f.close()
 
     em = discord.Embed()
     em.add_field(name='Notification', value='Notification added to go off **' + day.capitalize() + ', ' + time + '**')
 
-    await ctx.send(embed=em)
+    return em
 
-@bot.command()
-async def remove(ctx, noti):
+async def on_remove(guild: discord.Guild, author: discord.Member, noti: str):
 
-    if not has_perms(ctx.guild, ctx.author):
+    if not has_perms(guild, author):
         em = discord.Embed()
         em.add_field(name='Remove', value='You have no permissions to use this command')
-        await ctx.send(embed=em)
-        return
+        return em
 
-    f = open(get_guild_file(ctx.guild))
+    f = open(get_guild_file(guild))
     data = json.load(f)
     f.close()
 
@@ -300,21 +296,54 @@ async def remove(ctx, noti):
     for guild_noti in data['notifications']:
         if guild_noti['message'] == noti:
             data['notifications'].remove(guild_noti)
-            f = open(get_guild_file(ctx.guild), 'w')
+            f = open(get_guild_file(guild), 'w')
             f.write(json.dumps(data, indent=2))
             f.close()
             em.add_field(name='Notification', value='notifications: ' + noti + 'has been removed')
-            await ctx.send(embed=em)
-            return
+            return em
 
     em.add_field(name='Notification', value='Notification at ____ does not exist')
-    await ctx.send(embed=em)
+    return em
 
+#name='woah',
+#description='woajsodj',
+# @tree.command(guild=discord.Object(id=961988797422248057))
+# async def test(interaction: discord.Interaction): #, number: int, string: str):
+#     await interaction.response.send_message('woajsodaj')
+
+
+tree = app_commands.CommandTree(client=bot)
+
+@tree.command()
+async def list(interaction: discord.Interaction, all: str):
+    await interaction.response.send_message(embed=on_list(interaction.guild, interaction.channel, True if all == 'all' else False))
+
+@tree.command()
+async def help(interaction: discord.Interaction, command: str):
+    await interaction.response.send_message(embed=on_help(command))
+
+@tree.command()
+async def add(interaction: discord.Interaction, pings: str, message: str, day: str, time: str):
+    await interaction.response.send_message(embed=on_add(interaction.guild, interaction.user, interaction.channel, pings, message, day, time))
+
+@tree.command()
+async def remove(interaction: discord.Interaction, noti: str):
+    await interaction.response.send_message(embed=on_remove(interaction.guild, interaction.user, noti))
+
+@tree.command()
+async def set_utc_offset(interaction: discord.Interaction, offset: str):
+    await interaction.response.send_message(embed=on_set_utc_offset(interaction.guild, interaction.user, offset))
+
+@tree.command()
+async def set_perms_role(interaction: discord.Interaction, role_name: str):
+    await interaction.response.send_message(embed=on_set_perms_role(interaction.guild, interaction.user, role_name))
+
+@tree.command()
+async def now(interaction: discord.Interaction):
+    await interaction.response.send_message(embed=on_now(interaction.guild))
 
 def main():
-    # token here because doesnt matter rn
-    bot.run('OTYxNzU5ODUxODMzMzU2Mjk4.Yk9qqQ.Cp-RSjOzlRByBdj36F0hXRIknTw')
-    # bot.run(env.token)
+    bot.run(env.token)
 
 if __name__ == '__main__':
     main()
